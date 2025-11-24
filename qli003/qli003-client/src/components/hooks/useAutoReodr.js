5c630b60c0ad89d3078011c6f0632574992f8f66
David-W4220 <daweiwang@unomaha.edu>
2025-11-23 20:30:42 -0600

    Create useAutoReodr.js

---
+++ qli003/qli003-client/src/components/hooks/useAutoReodr.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+
+//hook for backend automated mailing service. 
+//Since AutoReodrLk.cs's a HostedService (singleton) thats NOT triggered by user, it does not need any endpoints
+export default function useAutoReodr() {
+  const [autoStatus, setAutoStatus] = useState(null)
+
+  useEffect(() => {
+    const handler = (e) => {
+      setAutoStatus(e.detail)
+    }
+
+    window.addEventListener("auto-reorder-status", handler)
+    return () => window.removeEventListener("auto-reorder-status", handler)
+  }, [])
+    
+  
+  // Auto-clear message
+  useEffect(() => {
+    if (!autoStatus) return
+
+    const timer = setTimeout(() => {setAutoStatus(null)}, 10000) // <- adjust timer here(currently 10 sec)
+
+    return () => clearTimeout(timer)}, [autoStatus])
+
+  return { autoStatus }
+}
